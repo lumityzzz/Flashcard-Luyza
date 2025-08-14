c205d110abaf1d6ad1346957eea27057aff6905a
Luyza <luyza.okawa@escola.pr.gov.br>
2025-08-13 23:23:15 -0300

    Add files via upload

---
+++ Luyza/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital da Turquia?',
+    'A capital da Turquia é Stambul'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Guatemala?',
+    'A capital da Guatemala é Cidade da Guatemala'
+)
+
+criaCartao(
+    'Geografia',
+    'Aonde fica o Canal do Panamá?',
+    'O Canal do Panamá fica na América Central no Panamá'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da República Dominicana?',
+    'A capital é Santo Domingo'
+)
